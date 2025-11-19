4b0f4036f7eda27e54a23f069373ae3319ba8125
Narendran S <122251349+SNarendranS@users.noreply.github.com>
2025-11-19 12:51:26 +0530

    Update loggedInSlice.js

--- frontend/src/redux/loggedInSlice.js
+++ frontend/src/redux/loggedInSlice.js
@@ -1,26 +1,47 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const storedUser =
+//   localStorage.getItem("userData") || sessionStorage.getItem("userData");
+
+// const loggedInSlice = createSlice({
+//   name: 'userToken',
+//   initialState: { value: storedUser },
+//   reducers: {
+//     setToken: (state, action) => {
+//       state.value = action.payload;
+//     },
+//     getToken: (state) => {
+//       return state.value;
+//     },
+//     removeToken: (state) => {
+//       state.value = null;
+//     },
+//     isLoggedIn: (state) => {
+//       return state.value !== null;
+//     }
+
+//   }
+// });
+
+// export const { setToken, getToken,removeToken, isLoggedIn } = loggedInSlice.actions;
+// export default loggedInSlice.reducer;
+
+
 import { createSlice } from '@reduxjs/toolkit';
 
 const storedUser =
   localStorage.getItem("userData") || sessionStorage.getItem("userData");
 
 const loggedInSlice = createSlice({
-  name: 'userToken',
+  name: 'isLoggedIn',
   initialState: { value: storedUser },
   reducers: {
-    setToken: (state, action) => {
+    setIsLoggedIn: (state, action) => {
       state.value = action.payload;
-    },
-    getToken: (state) => {
-      return state.value;
-    },
-    removeToken: (state) => {
-      state.value = null;
-    },
-    isLoggedIn: (state) => {
-      return state.value !== null;
     }
+
   }
 });
 
-export const { setToken, getToken,removeToken, isLoggedIn } = loggedInSlice.actions;
+export const { setIsLoggedIn } = loggedInSlice.actions;
 export default loggedInSlice.reducer;
